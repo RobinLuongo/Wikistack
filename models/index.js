7352f1ddaf5735f3aeb8b7e328a3342c7737b213
Jeffrey Cheung <jeffreyy.cheungg@gmail.com>
2017-11-10 17:23:09 -0500

    app.js still errors

--- models/index.js
+++ models/index.js
@@ -1,5 +1,19 @@
 var Sequelize = require('sequelize');
 var db = new Sequelize('postgres://localhost:5432/wikistack');
 
+const Page = db.define('PAGE', {
+	title: Sequelize.STRING,
+	urlTitle: Sequelize.STRING,
+	content: Sequelize.TEXT,
+	status: Sequelize.ENUM('open', 'closed')
+}
 
-//test change
+const User = db.define('USER', {
+	name: Sequelize.STRING;
+	email: Sequelize.STRING;
+})
+
+module.exports = {
+	Page: Page,
+	User: User
+}
